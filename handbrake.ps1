82f774aca6b521c017208e9761aa72ee0a01f263
Sam Hoyt <samh@Hoyt.net>
2019-05-25 06:52:46 -0500

    comment changes

--- handbrake.ps1
+++ handbrake.ps1
@@ -2,17 +2,20 @@
  
 $finished = $location + "\" + "finished";
 
-md $finished;
+mkdir $finished;
 
-$handbrakecli = "" # Path to HandbrakeCLI
- 
-$filter = "*.mkv" # What video format to search for. Can't figure out how to do multiple.
- 
-$arguments = "-e nvenc_h265 --encoder-preset slow -q 22 -E copy --comb-detect=fast --decomb=bob" # HandbrakeCLI arguments. -i and -o are handled elsewhere
+# Path to HandbrakeCLI
+$handbrakecli = "" 
+
+# What video format to search for. Can't figure out how to do multiple.
+$filter = "*.mkv" 
+
+# HandbrakeCLI arguments. -i and -o are handled elsewhere
+$arguments = "-e nvenc_h265 --encoder-preset slow -q 22 -E copy --comb-detect=fast --decomb=bob" 
  
 $sizel = 0
 $filelist = Get-ChildItem $location -filter $filter -exclude "_*" -recurse
-$num = $filelist | measure
+$num = $filelist | Measure-Object
 $filecount = $num.count
 $sizea = Get-ChildItem $location -recurse | Measure-Object -Sum Length
 $sizeb = "{0:N3}" -f ($sizea.sum/1gb)
@@ -20,6 +23,7 @@ $i = 0;
 ForEach ($file in $filelist)
 {
     $i++;
+
     # The two lines below automatically create the input and output file paths for HandbrakeCLI. It is not recommended to change this unless you know what you're doing.
     $oldfile = $file.DirectoryName + "\" + $file.BaseName + $file.Extension;
     $newfile = $finished + "\" + $file.BaseName + ".mp4"; # Change ".mp4" to $file.Extension to keep original filetype
@@ -49,7 +53,6 @@ ForEach ($file in $filelist)
 	$sizei = "{0:N3}" -f ($sizeh.sum/1gb)
 	$sizel = $sizel + ($sizeg - $sizei)
 	$sizee = $sizeb - $sizel
- 
 }
 $sizec = Get-ChildItem $finished -recurse | Measure-Object -Sum Length
 $sized = "{0:N3}" -f ($sizec.sum/1gb)
